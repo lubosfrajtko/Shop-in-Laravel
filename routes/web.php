22a077a900bd0b1bbdd4dd8398b6d9a522874c94
Much4puk <lubos.frajtko@centrum.sk>
2021-01-24 16:34:18 +0100

    zmenil som redirect po loginu

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
